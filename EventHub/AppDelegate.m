320799ccb2ce072f35e207932af36d178c607427
revintec <caoyunbin001@126.com>
2014-12-31 00:45:22 +0800

    fuck all apple's female, and male, for their "undocumented" api
    AXClassName not working, but it seems working fine in
    AccessibilityInspector(not the open source version, the DeveloperTools
    version)

--- EventHub/AppDelegate.m
+++ EventHub/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <objc/runtime.h>
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface AppDelegate()
 @property(weak)IBOutlet NSWindow*window;
@@ -15,8 +17,36 @@
 @end
 
 @implementation AppDelegate
+static inline OSStatus _GetProcessForPID(pid_t pid,ProcessSerialNumber*psn){
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    return GetProcessForPID(pid,psn);
+#pragma clang diagnostic pop
+}
+bool strokeKeycodeWithModifier(ProcessSerialNumber*psn,CGEventFlags modifiers,CGKeyCode key){
+    CGEventRef kd=CGEventCreateKeyboardEvent(nil,key,true);
+    CGEventRef ku=CGEventCreateKeyboardEvent(nil,key,false);
+    if(!kd||!ku){
+        if(kd)CFRelease(kd);
+        if(ku)CFRelease(ku);
+        return false;
+    }
+    CGEventSetFlags(kd,modifiers);
+    CGEventSetFlags(ku,modifiers);
+    if(psn){
+        CGEventPostToPSN(psn,kd);
+        CGEventPostToPSN(psn,ku);
+    }else{
+        CGEventPost(kCGSessionEventTap,kd);
+        CGEventPost(kCGSessionEventTap,ku);
+    }CFRelease(kd);CFRelease(ku);
+    return true;
+}
 
 bool optFilterCapslock;
+bool optFilterCmdDelFinder;
+    bool doptFinderFg;
+    ProcessSerialNumber psnFinder;
 
 bool smallHalt;CGEventFlags cachedEvFlags;
 -(void)unhalt{smallHalt=false;}
@@ -33,6 +63,7 @@ CGEventRef eventCallback(CGEventTapProxy proxy,CGEventType type,CGEventRef event
                 CGEventFlags diff=cachedEvFlags^newFlags;
                 cachedEvFlags=newFlags;
                 if(diff&kCGEventFlagMaskAlphaShift&&!(newFlags&kCGEventFlagMaskAlphaShift)){
+                    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(unhalt)object:self];
                     smallHalt=true;
                     CGEventPost(kCGSessionEventTap,self.kd);
                     CGEventPost(kCGSessionEventTap,self.ku);
@@ -42,9 +73,53 @@ CGEventRef eventCallback(CGEventTapProxy proxy,CGEventType type,CGEventRef event
         }
     }
     
+    if(optFilterCmdDelFinder&&doptFinderFg){
+        if(type==kCGEventKeyDown||type==kCGEventKeyUp){
+            int64_t keycode=CGEventGetIntegerValueField(event,kCGKeyboardEventKeycode);
+            if(keycode==kVK_Delete){
+                CGEventFlags f=CGEventGetFlags(event)&NSDeviceIndependentModifierFlagsMask;
+                f&=~kCGEventFlagMaskAlphaShift;
+                if(f==kCGEventFlagMaskCommand){
+                    if(type==kCGEventKeyUp){
+                        CFTypeRef elem=AXUIElementCreateSystemWide();
+                        if(AXUIElementCopyAttributeValue(elem,kAXFocusedUIElementAttribute,&elem))return nil;
+                        CFTypeRef role;if(AXUIElementCopyAttributeValue(elem,kAXRoleAttribute,&role))return nil;
+                        if(!CFEqual(kAXTextFieldRole,role)){NSLog(@"ignore CmdDel on non TextField role in Finder");return nil;}
+                        CFTypeRef className;
+                        if(AXUIElementCopyAttributeValue(elem,(CFTypeRef)@"AXClassName",&className)){
+                            NSLog(@"unable to get AXClassName, fuck apple x1");
+                            strokeKeycodeWithModifier(&psnFinder,kCGEventFlagMaskShift,kVK_Home);
+                            strokeKeycodeWithModifier(&psnFinder,0,kVK_Delete);
+                        }else{
+                            NSLog(@"AXClassName: %@",className);
+                            if(CFEqual(@"TShrinkToFitTextView",className)){
+                                strokeKeycodeWithModifier(&psnFinder,kCGEventFlagMaskShift,kVK_Home);
+                                strokeKeycodeWithModifier(&psnFinder,0,kVK_Delete);
+                            }else NSLog(@"not a TShrinkToFitTextView");
+                        }
+                    }return nil;
+                }
+            }
+        }
+    }
+    
     return event;
 }
 
+-(void)someotherAppGotActivated:(NSNotification*)notification{
+    NSDictionary*_n=[notification userInfo];if(!_n)return;
+    NSRunningApplication*ra=[_n objectForKey:NSWorkspaceApplicationKey];if(!ra)return;
+    NSString*name=[ra localizedName];
+    if([@"Finder" isEqual:name]){
+        // _GetProcessForPID returns OSStatus if error
+        // set doptFinderFg only when no error
+        doptFinderFg=!_GetProcessForPID([ra processIdentifier],&psnFinder);
+        if(!doptFinderFg){
+            NSLog(@"unable to get Finder's PSN, will disable CmdDel filter in Finder");
+            AudioServicesPlayAlertSound(kSystemSoundID_UserPreferredAlert);
+        }
+    }else doptFinderFg=false;
+}
 -(void)fatalWithText:(NSString*)msg{
     NSRunningApplication*ra=[NSRunningApplication currentApplication];
     NSAlert*alert=[NSAlert new];
@@ -56,6 +131,38 @@ CGEventRef eventCallback(CGEventTapProxy proxy,CGEventType type,CGEventRef event
     [NSApp terminate:self];
 }
 -(IBAction)quitApp:(id)sender{[NSApp terminate:sender];}
+-(void)dumpInfo:(id)obj{
+    Class clazz=[obj class];
+    u_int count;
+    Ivar*ivars=class_copyIvarList(clazz,&count);
+    NSMutableArray*ivarArray=[NSMutableArray arrayWithCapacity:count];
+    for(int i=0;i<count;++i){
+        const char*ivarName=ivar_getName(ivars[i]);
+        [ivarArray addObject:[NSString stringWithCString:ivarName encoding:NSUTF8StringEncoding]];
+    }free(ivars);
+    
+    objc_property_t*properties=class_copyPropertyList(clazz,&count);
+    NSMutableArray*propertyArray=[NSMutableArray arrayWithCapacity:count];
+    for(int i=0;i<count;++i){
+        const char*propertyName=property_getName(properties[i]);
+        [propertyArray addObject:[NSString stringWithCString:propertyName encoding:NSUTF8StringEncoding]];
+    }free(properties);
+    
+    Method*methods=class_copyMethodList(clazz,&count);
+    NSMutableArray*methodArray=[NSMutableArray arrayWithCapacity:count];
+    for(int i=0;i<count;++i){
+        SEL selector=method_getName(methods[i]);
+        const char*methodName=sel_getName(selector);
+        [methodArray addObject:[NSString stringWithCString:methodName encoding:NSUTF8StringEncoding]];
+    }free(methods);
+    
+    NSDictionary*classDump=[NSDictionary dictionaryWithObjectsAndKeys:
+                               ivarArray,@"ivars",
+                               propertyArray,@"properties",
+                               methodArray,@"methods",
+                               nil];
+    NSLog(@"%@", classDump);
+}
 -(void)applicationDidFinishLaunching:(NSNotification*)aNotification{
     if(!AXIsProcessTrusted()){
         [self.window close];
@@ -72,6 +179,8 @@ CGEventRef eventCallback(CGEventTapProxy proxy,CGEventType type,CGEventRef event
         return;
     }
     self.kd=kd;self.ku=ku;
+    NSNotificationCenter*ncc=[[NSWorkspace sharedWorkspace]notificationCenter];
+    [ncc addObserver:self selector:@selector(someotherAppGotActivated:)name:NSWorkspaceDidActivateApplicationNotification object:nil];
 }
 -(void)applicationWillTerminate:(NSNotification*)aNotification{
     if(self.eventTap){
@@ -82,6 +191,7 @@ CGEventRef eventCallback(CGEventTapProxy proxy,CGEventType type,CGEventRef event
 -(void)applicationDidResignActive:(NSNotification*)notification{
     CGEventMask interest=0;
     if(optFilterCapslock)interest|=CGEventMaskBit(kCGEventKeyDown)|CGEventMaskBit(kCGEventKeyUp)|CGEventMaskBit(kCGEventFlagsChanged);
+    if(optFilterCmdDelFinder)interest|=CGEventMaskBit(kCGEventKeyDown)|CGEventMaskBit(kCGEventKeyUp);
     
     if(interest){
         self.eventTap=CGEventTapCreate(kCGSessionEventTap,kCGHeadInsertEventTap,kCGEventTapOptionDefault,interest,(CGEventTapCallBack)eventCallback,(__bridge void*)(self));
@@ -105,5 +215,6 @@ CGEventRef eventCallback(CGEventTapProxy proxy,CGEventType type,CGEventRef event
     }
     // TODO add settings panel instead of hard-code options
     optFilterCapslock=true;
+    optFilterCmdDelFinder=true;
 }
 @end
